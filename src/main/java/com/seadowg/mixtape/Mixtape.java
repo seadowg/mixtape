3605dcb329f71e4275cc6d043e3bd872cf0cb8a2
Callum Stott <callum@seadowg.com>
2014-05-28 00:53:02 +0100

    Add template rendering

--- src/main/java/com/seadowg/mixtape/Mixtape.java
+++ src/main/java/com/seadowg/mixtape/Mixtape.java
@@ -1,17 +1,32 @@
 package com.seadowg.mixtape;
 
 import static spark.Spark.*;
-import spark.*;
+
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+
+import java.io.StringWriter;
+import java.io.IOException;
+import java.lang.RuntimeException;
+
 
 public class Mixtape {
   public static void main(String[] args) {
     externalStaticFileLocation("/var/www/public");
 
-    get(new Route("/") {
-      @Override
-      public Object handle(Request request, Response response) {
-        return "Hello World";
+    get("/", (req, res) -> {
+      MustacheFactory mustacheFactory = new DefaultMustacheFactory("templates");
+      Mustache mustache = mustacheFactory.compile("index.mustache");
+      StringWriter stringWriter = new StringWriter();
+
+      try {
+        mustache.execute(stringWriter, null).close();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
       }
+
+      return stringWriter.toString();
     });
   }
 }
